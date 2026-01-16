c275604ec8755224052c679736372f189b7b959e
sumit patil <sumitpatil9055@gmail.com>
2026-01-16 22:46:55 +0530

    Your commit message

--- frontend/src/SearchApp.jsx
+++ frontend/src/SearchApp.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useFetchJsonForLocation } from "./Components/useFetchJsonForLocation";
 import MyNavbar from "./Components/Nav";
