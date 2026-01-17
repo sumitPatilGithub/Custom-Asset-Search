89931392d14c31463b0029b5ae78067a68fe2e65
sumit patil <sumitpatil9055@gmail.com>
2026-01-17 11:50:18 +0530

    add email in storage

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -29,7 +29,7 @@ export default function Dashboard() {
             }
             else{
               setProfilePic(localStorage.getItem("profilePic"));
-              setUserEmail(profileData.data.emailAddress)
+              setUserEmail(localStorage.getItem("email"))
             }
            }
            loadData()
